f30a69f2099022abf880ef790e0e9f1649bfc05b
Srujan Bethi <ssbethi@gmail.com>
2019-05-20 12:52:06 -0600

    Fixed semicolons

--- Server/db/migrations/20190516185530_initial.js
+++ Server/db/migrations/20190516185530_initial.js
@@ -3,27 +3,27 @@ exports.up = function(knex, Promise) {
   return Promise.all([
 
     knex.schema.createTable('listings', (table) => {
-      table.increments('id').primary()
-      table.string('title')
+      table.increments('id').primary();
+      table.string('title');
     }),
     knex.schema.createTable('amenities', (table) => {
-      table.increments('id').primary()
-      table.string('name')
-      table.string('photoUrl')
+      table.increments('id').primary();
+      table.string('name');
+      table.string('photoUrl');
     }),
     knex.schema.createTable('listings_amenities', (table) => {
-      table.increments('id').primary()
-      table.integer('amenity_id')
-      table.integer('listing_id')
-      table.foreign('listing_id').references('listings.id')
-      table.foreign('amenity_id').references('amenities.id')
+      table.increments('id').primary();
+      table.integer('amenity_id');
+      table.integer('listing_id');
+      table.foreign('listing_id').references('listings.id');
+      table.foreign('amenity_id').references('amenities.id');
     }),
-  ])
+  ]);
 };
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-    knex.schema.dropTable('listings_amenities')
+    knex.schema.dropTable('listings_amenities'),
     knex.schema.dropTable('listings'),
     knex.schema.dropTable('amenities'),
   ]);
