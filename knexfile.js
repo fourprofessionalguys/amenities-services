0ec063ab112b9139e159daaee1b361820350daf9
Srujan Bethi <ssbethi@gmail.com>
2019-05-24 11:50:24 -0600

    Modified files

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/airbnb_amenities',
+    connection: 'postgres://flamingeagle9000:rosie123@localhost/airbnb_amenities',
     migrations: {
       directory: 'Server/db/migrations'
     },
