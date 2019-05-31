42bfec363c6a48bb9a5134d0a569435ad2d15af6
Srujan Bethi <ssbethi@gmail.com>
2019-05-31 11:41:11 -0600

    Completed refactored every component to SASS

--- Client/components/modalBody.jsx
+++ Client/components/modalBody.jsx
@@ -1,79 +1,31 @@
 import React from 'react';
 import styled, {createGlobalStyle} from 'styled-components';
-
+import '../main.scss';
 import ModalComp from './modalComp.jsx';
 
 
 
 const ModalBodyContainer = styled.div`
-  display: ${props => props.show ? "block" : "none"};
-  position: absolute;
-  top: 0px;
-  right: 0px;
-  bottom: 0px;
-  left: 0px;
-  width: 100%;
-  height: 100%;
-  z-index: 9998;
-  background: rgb(255, 255, 255)
-`;
-
-const FlexContainer = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-
-const ModalContainer = styled.div`
-  position: absolute;
-  width: 664px;
-  top: 64px;
-  bottom: 0px;
-`;
-
-const TopBar = styled.div`
-  height: 64px;
-  width: 100%;
-  position: fixed;
-  border-bottom: 1px solid rgb(228, 228, 228);
-`;
-
-const BackButton = styled.button`
-  cursor: pointer;
-  background-color: transparent;
-  color: buttontext;
-  display: block;
-  border-width: 0px;
-  border-style: initial;
-  border-color: initial;
-  border-image: initial;
-  padding-left: 1em;
-  padding-top: 1.5em;
-`;
-
-const BackButtonImg = styled.svg`
-  height: 22px;
-  width: 22px;
-  display: block;
-  fill: rgb(72, 72, 72);
+  &&&&& {
+    display: ${props => props.show ? "block" : "none"};
+  }
 `;
 
 const ModalBody = ({hideModal, show, amenities, needs, special}) => {
-
-
   return (
-    <ModalBodyContainer show={show}>
-      <FlexContainer>
-        <TopBar>
-          <BackButton onClick={hideModal}>
-            <BackButtonImg>
+    <ModalBodyContainer id="ModalBodyContainer" show={show}>
+      <div id="FlexContainer">
+        <div id="TopBar">
+          <button id="BackButton" onClick={hideModal}>
+            <svg id="BackButtonImg">
               <path d="m13.7 16.29a1 1 0 1 1 -1.42 1.41l-8-8a1 1 0 0 1 0-1.41l8-8a1 1 0 1 1 1.42 1.41l-7.29 7.29z"></path>
-            </BackButtonImg>
-          </BackButton>
-        </TopBar>
-        <ModalContainer>
+            </svg>
+          </button>
+        </div>
+        <div id="ModalContainer">
           <ModalComp needs={needs} special={special}></ModalComp>
-        </ModalContainer>
-      </FlexContainer>
+        </div>
+      </div>
     </ModalBodyContainer>
   );
 };
--- Client/components/modalComp.jsx
+++ Client/components/modalComp.jsx
@@ -1,80 +1,34 @@
 import React from 'react';
-import styled, {createGlobalStyle} from 'styled-components';
+import '../main.scss';
 
-const ContainerSSB = styled.div`
-  width: 664px;
-  padding-top: 42px;
-`;
-
-const CategoryContainerSSB = styled.div`
-  padding-bottom: 32px;
-`;
-const HeaderSSB = styled.div`
-  font-size: 32px;
-  line-height: 36px;
-  letter-spacing: normal;
-  font-weight: 800;
-  color: rgb(72, 72, 72);
-  padding-top: 6px;
-`;
-
-const DescriptionSSB = styled.div`
-  font-size: 16px;
-  font-weight: 400;
-  padding-bottom: 32px;
-  margin-top: 8px;
-`;
-
-const AmenitySSB = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding-top: 20px;
-  padding-bottom: 20px;
-  border-top: 1px solid rgb(228, 228, 228);
-  font-size: 16px;
-  font-weight: 500;
-  color: #484848;
-`;
-
-const ImgSSB = styled.img`
-  width: 64px;
-  height: 64px;
-  max-width: 64px;
-  max-height: 64px;
-`;
-
-
-const NameWrapperSSB = styled.div`
-  padding-top: 12px;
-`;
 const ModalComp = ({needs, special}) => {
   return (
-    <ContainerSSB>
-      <HeaderSSB>
+    <div id="ContainerSSB">
+      <div className="HeaderSSB">
         Special to this place
-        <DescriptionSSB>This place has these unique amenities.</DescriptionSSB>
-      </HeaderSSB>
-      <CategoryContainerSSB>
+        <div className="DescriptionSSB">This place has these unique amenities.</div>
+      </div>
+      <div className="CategoryContainerSSB">
         {special.map((obj, i) => 
-          <AmenitySSB key={i}>
-            <NameWrapperSSB>{obj.name}</NameWrapperSSB>
-            <ImgSSB src={obj.photoUrl}></ImgSSB>
-          </AmenitySSB>
+          <div className="AmenitySSB" key={i}>
+            <div className="NameWrapperSSB">{obj.name}</div>
+            <img className="ImgSSB" src={obj.photoUrl}></img>
+          </div>
         )}
-      </CategoryContainerSSB>
-      <HeaderSSB>
+      </div>
+      <div className="HeaderSSB">
         Everything you need
-        <DescriptionSSB>This Airbnb Plus home comes with these amenities.</DescriptionSSB>
-      </HeaderSSB>
-      <CategoryContainerSSB>
+        <div className="DescriptionSSB">This Airbnb Plus home comes with these amenities.</div>
+      </div>
+      <div className="CategoryContainerSSB">
         {needs.map((obj, i) => 
-          <AmenitySSB key={i}>
-            <NameWrapperSSB>{obj.name}</NameWrapperSSB>
-            <ImgSSB src={obj.photoUrl}></ImgSSB>
-          </AmenitySSB>
+          <div className="AmenitySSB" key={i}>
+            <div className="NameWrapperSSB">{obj.name}</div>
+            <img className="ImgSSB" src={obj.photoUrl}></img>
+          </div>
         )}
-      </CategoryContainerSSB>
-    </ContainerSSB>
+      </div>
+    </div>
   );
 };
 
